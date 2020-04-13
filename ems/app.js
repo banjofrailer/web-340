54b6fb611e974b48e2008342d69df9af860de2e8
Sarah Kovar <sarahkovar@gmail.com>
2020-04-12 22:22:04 -0500

    exercise 8.2 and 8.3

--- ems/app.js
+++ ems/app.js
@@ -20,6 +20,12 @@ var path = require("path");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 var Employee = require("./models/employee");
+//require helmet for xss
+var helmet = require("helmet");
+//require body-parser, cookie-parser, and csurf
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var csrf = require("csurf");
 
 //mongo database connection
 var MongoDB = "mongodb+srv://user:tumbleweed@buwebdev-cluster-1-xhsyh.mongodb.net/test";
@@ -44,12 +50,34 @@ var app = express();
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(logger("short"));
+//use helmet for xss
+app.use(helmet.xssFilter());
+//use body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+//use cookie-parser
+app.use(cookieParser());
+//use csrf protection
+app.use(csrfProtection);
+app.use(function(request, response, next){
+  var token = request.csrfToken();
+  response.cookie("XSRF-TOKEN", token);
+  response.locals.csrfToken = token;
+  next();
+})
+//set up csrf protection
+var csrfProtection = csrf({cookie: true});
 
 app.get("/", function(request, response) {
   response.render("index", {
-    title: "Home page"
+    title: "Home page",
+    message: "XSS Prevention Example"
   });
 });
+
+app.post("/process", function(request, response){
+  console.log(request.body.firstName);
+  response.redirect("/");
+})
 
 http.createServer(app).listen(8080, function() {
   console.log("Application started on port 8080!");
